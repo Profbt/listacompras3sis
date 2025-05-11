691b285510f5637b0f0d5cf18ae8979cffac2275
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2025-05-11 17:32:28 -0300

    ss

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://wqrvadycionozczmqgoj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxcnZhZHljaW9ub3pjem1xZ29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0OTUzOTIsImV4cCI6MjA2MjA3MTM5Mn0.mMVAn7kJQH8bLhijfug2E0lMvpjRkKRE7CC29IFNNt4'
+  'https://nntiriosaaaumiaygddl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5udGlyaW9zYWFhdW1pYXlnZGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5OTQ3NjIsImV4cCI6MjA2MjU3MDc2Mn0.H_d1nTWC42Ki9xtTsCUQxMI_zdaigFBYPj5GaCkSZCs'
 )
